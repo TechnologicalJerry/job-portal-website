fb8768c3fbec73c9b46f47ef977260092462b92d
TechnologicalJerry <cserajatverma@gmail.com>
2025-10-29 23:04:24 +0530

    Add initial Express app setup
    Created the main Express application file with JSON parsing, user deserialization middleware, and routing.

---
+++ express-server/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import router from "./routes";
+import deserializeUser from "./middleware/deserializeUser";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(deserializeUser);
+
+app.use(router);
+
+export default app;
